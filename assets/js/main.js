0cad7ec5647e69004a272b95460947bf3aae9cde
Lohit Ashwa Vaswani <ashwalohit@gmail.com>
2026-04-15 17:16:31 +0100

    Version 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,12 @@
-const card = document.getElementById("profileCard");
+const card = document.getElementById("card");
 
-if (card) {
-  card.addEventListener("mousemove", (e) => {
-    const rect = card.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+document.addEventListener("mousemove", (e) => {
+  let x = (window.innerWidth / 2 - e.clientX) / 20;
+  let y = (window.innerHeight / 2 - e.clientY) / 20;
 
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
+  card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+});
 
-    const rotateY = ((x - centerX) / centerX) * 8;
-    const rotateX = -((y - centerY) / centerY) * 8;
-
-    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-6px)`;
-  });
-
-  card.addEventListener("mouseleave", () => {
-    card.style.transform = "rotateX(0deg) rotateY(0deg) translateY(0px)";
-  });
-}
+card.addEventListener("mouseleave", () => {
+  card.style.transform = "rotateY(0deg) rotateX(0deg)";
+});
